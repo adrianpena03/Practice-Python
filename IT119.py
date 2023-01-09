553ff2cfb8fd8907a9252f9c647cb9899c9fdf5a
Adrian <adrianpena03@gmail.com>
2023-01-08 19:41:14 -0500

    Separated different code into new file

---
+++ IT119.py
@@ -0,0 +1,34 @@
+
+def ITE119():
+    print("Hello, Professor!")
+
+def name():
+    print("My name is %s and i'm %d" % ('Adrian', 18))
+
+def subtraction(num1, num2):
+    num3 = num1 - num2
+    print (num3)
+
+name() 
+ITE119()
+subtraction(3, 2)
+
+macbook = {
+  "MacbookAir": "13 Inch M1",
+  "MacbookPro": "13 Inch M1",
+  "MacbookPro2": "14 Inch M1 Pro"
+}
+macbook["MacbookComp"] = 'iMac'
+
+print(macbook.get("MacbookAir"))
+print(macbook)
+macbook["MacbookPro2"] = 13
+
+macbook.pop("MacbookPro")
+for k,v in macbook.items():
+    print(k, '->', v)
+
+print(macbook)
+
+macbook.clear()
+print(macbook)
--- Personal.py
+++ Personal.py
@@ -1,52 +1,22 @@
 
-print("Hello, welcome to McDonald's.")
+print("\n\nHello, welcome to McDonald's.")
 
 name = input("What is your name? \n")
 
 print("Hello, " + name + ". Thank you for coming in today. \n\n")
 
-menu = "McChicken, Fries, Burger, Nuggets"
+menu = "McChickens, Fries, Burgers, Nuggets"
 
 print(name + ", the menu is " + menu + ". What would you like?")
 
 order = input()
 
-print("Sounds good " + name + ". We'll have your " + order + " ready for you done in a moment. \n\n\n\n")
+price = 8
 
+Quantity = input("How many " + order + " would you like from the menu? ")
 
-def ITE119():
-    print("Hello, Professor!")
+FinalPrice = price * int(Quantity)
 
-def name():
-    print("My name is %s and i'm %d" % ('Adrian', 18))
+print("That will be $" + str(FinalPrice))
 
-def subtraction(num1, num2):
-    num3 = num1 - num2
-    print (num3)
-
-name() 
-ITE119()
-subtraction(3, 2)
-
-macbook = {
-  "MacbookAir": "13 Inch M1",
-  "MacbookPro": "13 Inch M1",
-  "MacbookPro2": "14 Inch M1 Pro"
-}
-macbook["MacbookComp"] = 'iMac'
-
-print(macbook.get("MacbookAir"))
-print(macbook)
-macbook["MacbookPro2"] = 13
-
-macbook.pop("MacbookPro")
-for k,v in macbook.items():
-    print(k, '->', v)
-
-print(macbook)
-
-macbook.clear()
-print(macbook)
-
-
-
+print("\nSounds good " + name + ". We'll have your " + Quantity + " " + order + " ready for you in a moment. \n\n\n\n")
