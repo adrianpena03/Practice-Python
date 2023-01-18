551fc18af8ed03fa2c2758e01bd9477a85516156
Adrian <adrianpena03@gmail.com>
2023-01-17 20:55:06 -0500

    more code

--- Personal.py
+++ Personal.py
@@ -3,11 +3,22 @@ print("\n\nHello, welcome to McDonald's.")
 
 name = input("What is your name? \n")
 
-print("Hello, " + name + ". Thank you for coming in today. \n\n")
+
+
+if name == "Ben":
+    print("You're not welcome here evil Ben. Get out.")
+else:
+    print("Hello, " + name + ". Thank you for coming in today. \n\n")
+
+
+
 
 menu = "McChickens, Fries, Burgers, Nuggets"
 
 print(name + ", the menu is " + menu + ". What would you like?")
+
+
+
 
 order = input()
 
@@ -16,6 +27,9 @@ price = 8
 Quantity = input("How many " + order + " would you like from the menu? ")
 
 FinalPrice = price * int(Quantity)
+
+
+
 
 print("That will be $" + str(FinalPrice))
 
