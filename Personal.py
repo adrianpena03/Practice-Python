0d7923b315a4075dfa6b851f526875ec2d06de61
Adrian <adrianpena03@gmail.com>
2023-01-20 16:26:51 -0500

    adding logical operators

--- Personal.py
+++ Personal.py
@@ -5,16 +5,16 @@ name = input("What is your name? \n")
 
 
 
-if name == "Ben":
+if name == "Ben" or name == "Patricia" or name == "Loki":
     evil_status = input("Are you evil?\n")
-    if evil_status == "Yes":
-        print("You're not welcome here evil Ben. Get out.")
+    good_deeds = int(input("How many good deeds have you done today?\n"))
+    if evil_status == "Yes" and good_deeds < 4:
+        print("You're not welcome here evil " + name + ". Get out.")
         exit()
     else:
-        print("Oh, so you're one of the good Bens. Come on in!")
+        print("Come on in, " + name + "!")
 else:
     print("Hello, " + name + ". Thank you for coming in today. \n\n")
-
 
 
 
@@ -23,23 +23,21 @@ else:
 #print(name + ", the menu is " + menu + ". What would you like?")
 
 
-if order == "McChicken":
     
-
 
 #order = input()
 
-if order == "McChicken":
-    price = 3
-elif order == "Fries":
-    price = 2
-elif order == "Burger":
-    price = 5
-elif order == "Nuggets":
-    price = 1
-else:
-    print("Sorry, we don't have that here.")
-    price = 0
+#if order == "McChicken":
+#    price = 3
+#elif order == "Fries":
+#    price = 2
+#elif order == "Burger":
+#    price = 5
+#elif order == "Nuggets":
+#    price = 1
+#else:
+#    print("Sorry, we don't have that here.")
+#    price = 0
 
 #Quantity = input("How many " + order + " would you like from the menu? ")
 
@@ -48,6 +46,6 @@ else:
 
 
 
-print("That will be $" + str(FinalPrice))
+#print("That will be $" + str(FinalPrice))
 
-print("\nSounds good " + name + ". We'll have your " + Quantity + " " + order + " ready for you in a moment. \n\n\n\n")
+#print("\nSounds good " + name + ". We'll have your " + Quantity + " " + order + " ready for you in a moment. \n\n\n\n")
