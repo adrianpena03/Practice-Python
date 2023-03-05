2ca0e41341bc2a12bd669f462f23367901cd901e
Adrian <adrianpena03@gmail.com>
2023-03-05 16:00:18 -0500

    Commented out code, added conversion from list to string to list

--- Practice4Exam2.py
+++ Practice4Exam2.py
@@ -11,28 +11,30 @@
 
 # result = DoubleLetter("Adrian")
 
+# ----------------------------------------------------
+
 # Write a function called Eight_count(bin) that receives a string consisting of
 # only eights and nines as arguments and returns the biggest number of 
 # consecutive eights in the string
 
-bin = "88899988999888888"
+# bin = "998889"
 
-def Eight_Count(bin):
-    count = 0
-    biggest = 0
-    for i in bin:
-        if i == "8":
-            count += 1
-        else:
-            if count > biggest:
-                biggest = count
-            count = 0
-        if count > biggest:
-            biggest = count
-    return biggest            
+# def Eight_Count(bin):
+#     count = 0
+#     biggest = 0
+#     for i in bin:
+#         if i == "8":
+#             count += 1
+#         else:
+#             if count >= biggest:
+#                 biggest = count
+#             count = 0
+#         if count >= biggest:
+#             biggest = count
+#     return biggest            
 
-result = Eight_Count(bin)
-print(result)
+# result = Eight_Count(bin)
+# print(result)
 
 # def eight_count(bin):
 #      cons = 0
@@ -51,4 +53,23 @@ print(result)
 # result = eight_count(bin)
 # print(result)
 
-#----------------------------------------
+#------------------------------------------------
+courses = ['History', 'Math', 'Physics', 'CompSci']
+nums = [9, 7, 4, 5, 8, 1]
+
+# convert nums into list of strings
+num_strs = [str(num) for num in nums]
+print("Step 1:", num_strs)
+
+# join list of strings into a single string separated by ' - '
+num_str = ' - '.join(num_strs)
+print("Step 2:", num_str)
+
+# split string back into a list of strings
+lis = num_str.split(' - ')
+print("Step 3:", (lis))
+
+# convert list of strings back into list of integers
+lis = [int(num) for num in lis]
+
+print("Step 3:", lis)
