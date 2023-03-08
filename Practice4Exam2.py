583be366d7ea0ae3eee8d4143271e9c5be0ca031
Adrian <84482139+adrianpena03@users.noreply.github.com>
2023-03-08 00:50:04 -0500

    Added problems

--- Practice4Exam2.py
+++ Practice4Exam2.py
@@ -54,26 +54,48 @@
 # print(result)
 
 #------------------------------------------------
-courses = ['History', 'Math', 'Physics', 'CompSci']
-nums = [9, 7, 4, 5, 8, 1]
+# courses = ['History', 'Math', 'Physics', 'CompSci']
+# nums = [9, 7, 4, 5, 8, 1]
 
-# convert nums into list of strings
-num_strs = [str(num) for num in nums]
-print("Step 1:", num_strs)
+# # convert nums into list of strings
+# num_strs = [str(num) for num in nums]
+# print("Step 1:", num_strs)
 
-# join list of strings into a single string separated by ' - '
-num_str = ' - '.join(num_strs)
-print("Step 2:", num_str)
+# # join list of strings into a single string separated by ' - '
+# num_str = ' - '.join(num_strs)
+# print("Step 2:", num_str)
 
-# split string back into a list of strings
-lis = num_str.split(' - ')
-print("Step 3:", (lis))
+# # split string back into a list of strings
+# lis = num_str.split(' - ')
+# print("Step 3:", (lis))
 
-# convert list of strings back into list of integers
-lis = [int(num) for num in lis]
+# # convert list of strings back into list of integers
+# lis = [int(num) for num in lis]
 
-print("Step 3:", lis)
+# print("Step 3:", lis)
 # -------------------------------------------------
-word = "Hello World"
+# Write a function that receives a tuple as input. The tuple might include duplicates. 
+# Convert the tuple to a set which will remove duplicates automatically. 
+# Then convert the set to a list. Then sort the list. 
+# Then create a new list that includes the items in the previous list, each one duplicated as many times as its index. 
+# For instance, the first item in the list would appear only once in the new list, 
+   # the second item in the list would appear two times in the list, etc. 
+# Then it prints the new list and returns the new list. For instance, if the input is ('b', 'a', 'c', 'a', 'c'), 
+# the output should be ['a', 'b', 'b', 'c', 'c', 'c']. If the input is (8, 5, 6, 6), the output should be [5, 6, 6, 8, 8, 8].
 
-print(word[-1:-5:-1])
+# tup = ("a", "a", "a", "b", "c", "d", "e", "f", "g")
+
+# def Geometric(tup):
+#     tup_to_set = set(tup)
+#     set_to_lis = list(tup_to_set)
+#     set_to_lis.sort()
+
+#     new_list = []
+#     dup = 0
+#     for letter in set_to_lis:
+#         dup = dup + 1
+#         new_list.append(letter * dup)
+#     return new_list
+
+# print(Geometric(tup))
+#-----------------------------------------------------
