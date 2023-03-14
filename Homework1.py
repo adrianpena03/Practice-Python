cde9868c0e70d3afd6ac3cc5770661829b4e3e38
Adrian <84482139+adrianpena03@users.noreply.github.com>
2023-03-14 12:56:33 -0400

    Added class comment

--- Homework1.py
+++ Homework1.py
@@ -1,8 +1,20 @@
-a = int(input('Please enter an integer. \n'))
+# a = int(input('Please enter an integer. \n'))
 
-if a >= 100:
-    print("Large")
-elif 20 < a < 100:
-    print("Average")
-elif a <= 20:
-    print("Small")
+# if a >= 100:
+#     print("Large")
+# elif 20 < a < 100:
+#     print("Average")
+# elif a <= 20:
+#     print("Small")
+
+class Point:
+    def move(self):
+        print("move")
+
+    def draw(self):
+        print("draw")
+
+
+point1 = Point()
+
+point1.draw()
