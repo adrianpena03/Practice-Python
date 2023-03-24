2f7b7009445a4d1504b1993f8707f46d176c59fc
Adrian <84482139+adrianpena03@users.noreply.github.com>
2023-03-23 20:17:07 -0400

    Added code for 14

--- 14-ListInDictionary.py
+++ 14-ListInDictionary.py
@@ -1,9 +1,17 @@
+# Define list_1 = ['Ten', 'Twenty', 'Thirty'] and list_2 = [10, 20, 30], then write a program 
+# that uses a for loop to create a dictionary called dict whose keys are items from list_1
+# and whose values are items from list_2 and then prints the dict. 
+
 list_1 = ['Ten', 'Twenty', 'Thirty']
-list_2 = list_2 = [10, 20, 30]
+list_2 = [10, 20, 30]
 
-dict = {
+my_dict = {}
 
-}
+for i in range(len(list_1)):
+    key = list_1[i]
+    value = list_2[i]
+    my_dict[key] = value
 
-for l1, l2 in dict:
-    print("Hello")
+
+# Testing Code
+print(my_dict)
