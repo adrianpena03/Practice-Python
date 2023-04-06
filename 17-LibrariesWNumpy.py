b557f315af35804254fd4262a345fb559fb9fa05
Adrian Pena <adrianpena@arkham.lan>
2023-04-06 14:13:01 -0400

    Added 17, importing numpy

---
+++ 17-LibrariesWNumpy.py
@@ -0,0 +1,14 @@
+# Write a program to delete the second column from the following array and 
+# insert the following new column in its place, and print the result.
+
+# original_array = numpy.array(
+# [34 43 73]
+# [82 22 12]
+# [53 94 66]])
+# new_column = numpy.array([[10,10,10]])
+# After insertion, your array should look like:
+# [[34 10 73]
+# [82 10 12]
+# [53 10 66]]
+
+import numpy as np
