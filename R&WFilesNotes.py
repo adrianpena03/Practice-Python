61a395333dc402711f01bf608ed5e40d3e0e6d28
Adrian Pena <adrianpena@arkham.lan>
2023-04-18 22:41:30 -0400

    R&W uncommented

--- R&WFilesNotes.py
+++ R&WFilesNotes.py
@@ -1,13 +1,13 @@
-# # Approach 1:
-# f = open("Exfile.txt", "r")
-# # Do something
-# f.close()
+# Approach 1:
+f = open("Exfile.txt", "r")
+# Do something
+f.close()
 
 # Approach 2
-# with open("Exfile.txt", "r") as f:
-#     pass
+with open("Exfile.txt", "r") as f:
+    pass
     # Do something with the file
-#if you start aa new line here, without indentation, it will close the file
+# if you start aa new line here, without indentation, it will close the file
 
 # r: read?
 # w: overwriting?
@@ -16,30 +16,30 @@
 # a: append, opens file for purpose of writing, ONLY ADDS ONTO. (If file doesn't exist, it will add. Will not return error if doesn't error)
 # a+: also allows you to read from file
 
-# with open("Exfile.txt") as f:
-#     # print(f.name)
-#     # var = f.read() # reads entire content of file into one string variable. Data type is STR
-#     # whole_content = f.read()
-#     # print (whole_content)
-#     list_of_Lines = f.readlines() # Reads a whole line, it is a list
-#     for i in range(len(list_of_Lines)):
-#         list_of_lines[i] = list_of_lines[i].rstrip("\n")
-#     print(var)
+with open("Exfile.txt") as f:
+    # print(f.name)
+    # var = f.read() # reads entire content of file into one string variable. Data type is STR
+    # whole_content = f.read()
+    # print (whole_content)
+    list_of_lines = f.readlines() # Reads a whole line, it is a list
+    for i in range(len(list_of_lines)):
+        list_of_lines[i] = list_of_lines[i].rstrip("\n")
+    print(list_of_lines)
 
-# with open("Exfile.txt", "r") as f:
-#     first_line = f.readline() # returns only one line of the file as a string
-#     print(first_line) # prints first line
-#     second_line = f.readline()
-#     print(second_line) # prints second line
+with open("Exfile.txt", "r") as f:
+    first_line = f.readline() # returns only one line of the file as a string
+    print(first_line) # prints first line
+    second_line = f.readline()
+    print(second_line) # prints second line
 
-# with open("Exfile.txt", "r") as f:
-#     read_lines = f.readline()
-#     for ln in f:
-#         print(ln) # prints all lines
+with open("Exfile.txt", "r") as f:
+    read_lines = f.readline()
+    for ln in f:
+        print(ln) # prints all lines
     
-# with open("Exfile.txt", "r") as f:
-#     f.write("Hello you!") # Writes to file
-#     f.write("Goodbye") # Writes after previous write, no spaces
+with open("Exfile.txt", "r") as f:
+    f.write("Hello you!") # Writes to file
+    f.write("Goodbye") # Writes after previous write, no spaces
 
 with open("Exfile.txt", "r") as r_f:
     with open("Exfile1.txt", "w") as w_f:
