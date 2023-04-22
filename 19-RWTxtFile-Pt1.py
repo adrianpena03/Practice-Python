91b199ebdcb8ad164605c664a3ce84149851a7ae
Adrian Pena <adrianpena@arkham.lan>
2023-04-22 16:15:28 -0400

    Added 19 and 20 problem sets

---
+++ 19-RWTxtFile-Pt1.py
@@ -0,0 +1,8 @@
+# Prepare a txt file that has five lines with three words on each line, 
+# separated with a space. Write a program that reads the txt file and 
+# makes three lists out of it and prints the three lists. The elements of 
+# the first list are the first word on each line. The elements of the second 
+# list are the second word on each line. The elements of the third list are the 
+# third word on each line. Each list has five elements. Make sure that there 
+# are no new line characters in the elements.
+
---
+++ 20-RWTextFile-Pt2.py
@@ -0,0 +1,6 @@
+# Make a txt file and write 10 lines of text in it, anything.
+# Read the file in Python and write a new txt file using your code 
+# that includes every other line in the first text file. Therefore, 
+# the new txt file has five lines which are line 1, line 3, line 5, 
+# line 7, and line 9 in the first txt file.
+
