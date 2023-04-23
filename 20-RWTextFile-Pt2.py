747a3fe72e361ebc939a0be4ef642dc7083a6991
Adrian <adrianpena03@gmail.com>
2023-04-23 19:02:48 -0400

    Added solutions for 20

--- 20-RWTextFile-Pt2.py
+++ 20-RWTextFile-Pt2.py
@@ -4,10 +4,15 @@
 # the new txt file has five lines which are line 1, line 3, line 5, 
 # line 7, and line 9 in the first txt file.
 
-with ("20File.txt", "r") as f:
-    count = 0
-    new_list = []
-    for ln in f:
-        count += 1
-        if count % 2 != 0:
-            new_list.append(ln)
+with open("20File.txt", "r") as f:
+    with open("20New.txt", "w") as t:
+        count = 0
+        for ln in f:
+            count += 1
+            if count % 2 != 0:
+                t.write(ln)
+
+# Testing Code
+with open("20New.txt", "r") as t:
+    contents = t.read()
+    print(contents)
