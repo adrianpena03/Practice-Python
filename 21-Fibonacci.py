1e16dad53d118e6a7a9961ac788090440f4e6c2e
Adrian <adrianpena03@gmail.com>
2023-04-27 23:43:40 -0400

    Added 21 and 22 problem setes

---
+++ 21-Fibonacci.py
@@ -0,0 +1,9 @@
+# The Fibonacci Sequence is a series of numbers. 
+# The first two numbers are 0 and 1. The next number is found by 
+# adding up the two numbers before it. For example, 0, 1, 1, 2, 3, 5, 
+# 8, 13, 21. The next number in this series is 13+21 = 34. 
+# Write a program that receives an integer from the user, 
+# called n, and prints out Fibonacci series up to n terms. 
+# For example, if the user types 5, your program should print out 
+# 0, 1, 1, 2, 3.
+
---
+++ 22-2DMatrix.py
@@ -0,0 +1,11 @@
+# Introduction: A magic square is a 2D matrix composed of n^2 integers where 
+# n is the length of one row or column. In a magic square each row, 
+# each column, and the two diagonals must sum to the same value.
+
+# Problem: Given a 2D matrix, your program must determine 
+# if the square is magic or not magic. It will print "magic" or 
+# "non magic" accordingly.
+# Input: Your program will first take an input n, representing 
+# the length of one row of the matrix from the user. It will then 
+# take n lines of input containing n integers separated by spaces from the user.
+
